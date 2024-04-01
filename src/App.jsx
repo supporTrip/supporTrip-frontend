fae4bf366ebaf12c33d24b459938a9adc99cfd72
eunhye park <lime1pm@naver.com>
2024-04-02 07:05:12 +0900

    Feat: 기본 페이지들 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
+import Home from './pages/Home'
+import Account from './pages/Account'
+import Exchange from './pages/Exchange'
+import FlightInsurance from './pages/FlightInsurance'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import MyPage from './pages/MyPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      \
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/flight-insurance" element={<FlightInsurance />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/FlightInsurance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FlightInsurance = () => {
+  return (
+    <div>
+      FlightInsurance
+    </div>
+  )
+}
+
+export default FlightInsurance
