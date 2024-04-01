c43c4ed49e9830022c0f5e20af4cccf5dcacaedf
rlaxoqkf <rlaxoqkf96@gmail.com>
2024-04-01 16:57:46 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,87 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      \
-    </>
+      <>
+        <div class="section-banner">
+      <div id="star-1">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-2">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-3">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-4">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-5">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-6">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+
+      <div id="star-7">
+      <div class="curved-corner-star">
+        <div id="curved-corner-bottomright"></div>
+        <div id="curved-corner-bottomleft"></div>
+      </div>
+      <div class="curved-corner-star">
+        <div id="curved-corner-topright"></div>
+        <div id="curved-corner-topleft"></div>
+      </div>
+      </div>
+      </div>
+
+      </>
   )
 }
 
