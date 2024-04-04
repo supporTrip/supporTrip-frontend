3abdcc060a12932a5e9b07757284718a513c16f9
eheh10 <lime1pm@naver.com>
2024-04-04 14:54:23 +0900

    Style: 코드 스타일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Account from './pages/Account'
@@ -7,48 +8,68 @@ import FlightInsurance from './pages/FlightInsurance'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import MyPage from './pages/MyPage'
-import DefaultLayout from "./components/layout/DefaultLayout"
+import DefaultLayout from './components/layout/DefaultLayout'
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <DefaultLayout>
-            <Home />
-          </DefaultLayout>
-        } />
-        <Route path="/account" element={
-          <DefaultLayout>
-            <Account />
-          </DefaultLayout>
-        } />
-        <Route path="/exchange" element={
-          <DefaultLayout>
-            <Exchange />
-          </DefaultLayout>
-        } />
-        <Route path="/flight-insurance" element={
-          <DefaultLayout>
-            <FlightInsurance />
-          </DefaultLayout>
-        } />
-        <Route path="/signin" element={
-          <DefaultLayout hasNavbar={false} hasFooter={false}>
-            <SignIn />
-          </DefaultLayout>
-        } />
-        <Route path="/signup" element={
-          <DefaultLayout hasNavbar={false} hasFooter={false}>
-            <SignUp />
-          </DefaultLayout>
-        } />
-        <Route path="/mypage" element={
-          <DefaultLayout>
-            <MyPage />
-          </DefaultLayout>
-        } />
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <Home />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <DefaultLayout>
+              <Account />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/exchange"
+          element={
+            <DefaultLayout>
+              <Exchange />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/flight-insurance"
+          element={
+            <DefaultLayout>
+              <FlightInsurance />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <DefaultLayout hasNavbar={false} hasFooter={false}>
+              <SignIn />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <DefaultLayout hasNavbar={false} hasFooter={false}>
+              <SignUp />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            <DefaultLayout>
+              <MyPage />
+            </DefaultLayout>
+          }
+        />
       </Routes>
     </Router>
   )
--- src/chakra-theme.js
+++ src/chakra-theme.js
@@ -1,10 +1,10 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
     body: "'Pretendard-Regular', 'Pretendard-Black', 'Pretendard-ExtraBold', 'Pretendard-Bold', 'Pretendard-SemiBold', 'Pretendard-Medium', 'Pretendard-Regular', 'Pretendard-Light', 'Pretendard-ExtraLight', 'Pretendard-Thin', sans-serif",
     heading: "'Pretendard-Regular', sans-serif",
   },
-});
+})
 
-export default theme;
+export default theme
