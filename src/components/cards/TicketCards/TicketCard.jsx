704bbad4e8ec3b592ba4d49ef5b502bc0b689f92
eunhye park <lime1pm@naver.com>
2024-04-08 02:54:00 +0900

    Feat: ST-36 거래 중인 환전 내역 보여주기

---
+++ src/components/cards/TicketCards/TicketCard.jsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  SimpleGrid,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+} from '@chakra-ui/react'
+
+const TicketCard = ({
+  title,
+  originCash,
+  originCurrency,
+  exchangeCash,
+  exchangeCurrency,
+  originCentury,
+  exchangeCentury,
+  ticket,
+  type,
+  createdAt,
+  endDate,
+}) => {
+  return (
+    <Grid
+      minWidth={'700px'}
+      templateColumns={'2fr 1fr'}
+      border={'1px solid'}
+      borderTop={0}
+      borderBottom={0}
+      borderColor={'gray.200'}
+      borderRadius={'8px'}
+      // fontFamily={'pretendard-bold'}
+    >
+      <Text
+        p={4}
+        bg={'blue.400'}
+        color={'white'}
+        borderRight={'1.5px dashed'}
+        borderColor={'gray.200'}
+        borderTopRadius={'8px'}
+      >
+        {title}
+      </Text>
+      <Text
+        p={4}
+        bg={'blue.400'}
+        color={'white'}
+        borderTopRadius={'8px'}
+        align={'right'}
+      >
+        {createdAt}
+      </Text>
+      <Flex p={4} borderRight={'1.5px dashed'} borderColor={'gray.300'}>
+        <Box flex={1}>
+          <Text>원금</Text>
+          <Box>
+            <Text as="span">{originCash}</Text>
+            <Text as="span">{originCurrency}</Text>
+          </Box>
+        </Box>
+        <Box flex={1}>
+          <Text>환전금</Text>
+          <Box>
+            <Text as="span">{exchangeCash}</Text>
+            <Text as="span">{exchangeCurrency}</Text>
+          </Box>
+        </Box>
+      </Flex>
+      <Box p={4}>
+        <Text>티켓번호</Text>
+        <Text>{ticket}</Text>
+      </Box>
+      <Flex p={4} borderRight={'1.5px dashed'} borderColor={'gray.300'}>
+        <Box flex={1}>
+          <Text>기준 국가</Text>
+          <Text>{originCentury}</Text>
+        </Box>
+        <Box flex={1}>
+          <Text>대상 국가</Text>
+          <Text>{exchangeCentury}</Text>
+        </Box>
+      </Flex>
+      <Box p={4}>
+        <Text>거래 유형</Text>
+        <Text>{type}</Text>
+      </Box>
+      <GridItem
+        p={4}
+        alignSelf={'center'}
+        borderRight={'1.5px dashed'}
+        borderColor={'gray.300'}
+      >
+        <Slider
+          defaultValue={30}
+          isReadOnly={true}
+          isDisabled={true}
+          cursor={'default'}
+          focusThumbOnChange={'false'}
+        >
+          <SliderTrack bg="blue.100">
+            <SliderFilledTrack bg="blue.300" />
+          </SliderTrack>
+          <SliderThumb boxSize={1} border={0}>
+            <Box>{/* <Image src={Logo}></Image> */}</Box>
+          </SliderThumb>
+        </Slider>
+      </GridItem>
+      <Box p={2} pl={4}>
+        <Text>거래 기간</Text>
+        <Text>
+          {createdAt} ~ {endDate}
+        </Text>
+      </Box>
+      <Box
+        height={'40px'}
+        bg={'blue.400'}
+        borderRight={'1.5px dashed'}
+        borderColor={'gray.200'}
+        borderBottomRadius={'8px'}
+      ></Box>
+      <Box bg={'blue.400'} borderBottomRadius={'8px'}></Box>
+    </Grid>
+  )
+}
+
+export default TicketCard
--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -1,11 +1,100 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import TicketCard from '../components/cards/TicketCards/TicketCard'
 
+const dummy = [
+  {
+    title: '첫 휴가 기념 LA 여행',
+    ticket: '006 594269C1',
+    originCash: '500000',
+    originCurrency: '원',
+    originCentury: '대한민국',
+    exchangeCash: '371.47',
+    exchangeCurrency: '달러',
+    exchangeCentury: '미국',
+    createdAt: '2024.03.25',
+    endDate: '2024.12.24',
+    type: '적극투자형',
+  },
+  {
+    title: '퇴사 기념 일본~',
+    ticket: '006 594269C1',
+    originCash: '500000',
+    originCurrency: '원',
+    originCentury: '대한민국',
+    exchangeCash: '350000',
+    exchangeCurrency: '엔',
+    exchangeCentury: '일본',
+    createdAt: '2024.03.01',
+    endDate: '2024.12.10',
+    type: '안전형',
+  },
+  {
+    title: '놀러가고 싶다~~',
+    ticket: '006 594269C1',
+    originCash: '500000',
+    originCurrency: '원',
+    originCentury: '대한민국',
+    exchangeCash: '371.47',
+    exchangeCurrency: '달러',
+    exchangeCentury: '베트남',
+    createdAt: '2024.03.25',
+    endDate: '2024.12.24',
+    type: '적극투자형',
+  },
+  {
+    title: '야~~~호~~~~~',
+    ticket: '006 594269C1',
+    originCash: '500000',
+    originCurrency: '원',
+    originCentury: '대한민국',
+    exchangeCash: '371.47',
+    exchangeCurrency: '달러',
+    exchangeCentury: '태국',
+    createdAt: '2024.03.25',
+    endDate: '2024.12.24',
+    type: '위험투자형',
+  },
+]
 const Exchange = () => {
+  const exchanges = dummy
+
   return (
-    <>
-      Exchange
-    </>
+    <Flex mt={'30px'} direction={'column'} alignItems={'center'} color={'#333'}>
+      <Flex w={'70%'} alignItems={'center'} mb={'30px'}>
+        <Text mr={'15px'} fontWeight={'590'}>
+          환전 내역
+        </Text>
+        <Button
+          padding={'5px10px'}
+          bg={'#EFF6FF'}
+          color={'#2563EB'}
+          fontWeight={'normal'}
+        >
+          시작하기
+        </Button>
+      </Flex>
+      <Box w={'70%'}>
+        {exchanges.map((exchange, idx) => {
+          return (
+            <TicketCard
+              key={idx}
+              title={exchange.title}
+              originCash={exchange.originCash}
+              originCurrency={exchange.originCurrency}
+              exchangeCash={exchange.exchangeCash}
+              exchangeCurrency={exchange.exchangeCurrency}
+              originCentury={exchange.originCentury}
+              exchangeCentury={exchange.exchangeCentury}
+              ticket={exchange.ticket}
+              type={exchange.type}
+              createdAt={exchange.createdAt}
+              endDate={exchange.endDate}
+            />
+          )
+        })}
+      </Box>
+    </Flex>
   )
 }
-
 export default Exchange
