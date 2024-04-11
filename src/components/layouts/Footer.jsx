374d3e4a33648c7200e50ab632e914c856fc2283
eheh10 <lime1pm@naver.com>
2024-04-11 17:49:09 +0900

    Feat: ST-40 footer 수정

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,10 +1,27 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import Logo from '../../images/logo.svg'
 
 const Footer = ({ width = '100%' }) => {
   return (
-    <Box width={width} justifyContent={'center'}>
-      Footer
+    <Box
+      width={width}
+      justifyContent={'center'}
+      padding={'20px'}
+      color={'gray.200'}
+    >
+      <Flex gap={6} fontSize={'15px'} alignItems={'center'}>
+        <Flex alignItems={'center'}>
+          <Image src={Logo} w={'25px'} color={'gray.100'}></Image>
+          <Text ml={'2px'} fontSize={'19px'}>
+            서포트립
+          </Text>
+        </Flex>
+        <Text>서비스이용약관</Text>
+        <Text>개인정보처리방침</Text>
+        <Text>오픈뱅킹 서비스 이용약관</Text>
+        <Text>마이데이터 서비스 이용약관</Text>
+      </Flex>
     </Box>
   )
 }
