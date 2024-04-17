fd67b10c68470d9686b96dd3461815d7f5c0bba0
eheh10 <lime1pm@naver.com>
2024-04-17 11:24:28 +0900

    Feat: ST-111 푸터 디자인 수정

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,26 +1,24 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
-import Logo from '../../images/logo.svg'
+import { Link } from 'react-router-dom'
+import Logo from '../../images/logo-gray.svg'
 
 const Footer = ({ width = '100%' }) => {
   return (
-    <Box
-      width={width}
-      justifyContent={'center'}
-      padding={'20px'}
-      color={'gray.200'}
-    >
-      <Flex gap={6} fontSize={'15px'} alignItems={'center'}>
-        <Flex alignItems={'center'}>
-          <Image src={Logo} w={'25px'} color={'gray.100'}></Image>
-          <Text ml={'2px'} fontSize={'19px'}>
+    <Box width={width} justifyContent={'center'} py={'25px'} color={'gray.200'}>
+      <Flex gap={6} fontSize={'15px'} alignItems={'stretch'}>
+        <Flex flexShrink={0} alignItems={'center'}>
+          <Box>
+            <Image src={Logo} w={'25px'} color={'gray.100'}></Image>
+          </Box>
+          <Text alignItems={'center'} ml={'2px'} fontSize={'19px'}>
             서포트립
           </Text>
         </Flex>
-        <Text>서비스이용약관</Text>
-        <Text>개인정보처리방침</Text>
-        <Text>오픈뱅킹 서비스 이용약관</Text>
-        <Text>마이데이터 서비스 이용약관</Text>
+        <Link to={''}>서비스이용약관</Link>
+        <Link to={''}>개인정보처리방침</Link>
+        <Link to={''}>오픈뱅킹 서비스 이용약관</Link>
+        <Link to={''}>마이데이터 서비스 이용약관</Link>
       </Flex>
     </Box>
   )
