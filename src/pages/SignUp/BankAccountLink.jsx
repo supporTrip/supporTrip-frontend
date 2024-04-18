39c8a1ef1013de318c8135876f3ff2db5eff0315
yjlee0235 <yjlee0235@naver.com>
2024-04-18 16:16:22 +0900

    ST-110 Fix: 계좌 연결시 받는 약관 동의에 requestName 설정

--- src/pages/SignUp/BankAccountLink.jsx
+++ src/pages/SignUp/BankAccountLink.jsx
@@ -50,27 +50,27 @@ const banks = [
 const terms = [
   {
     title: '오픈뱅킹 자동계좌이체 약관 동의',
-    requestName: '',
+    requestName: 'open_banking_auto_transfer_consent',
     isNecessary: true,
   },
   {
     title: '오픈뱅킹 금융정보조회 약관 동의',
-    requestName: '',
+    requestName: 'open_banking_financial_info_inquiry_consent',
     isNecessary: true,
   },
   {
     title: '금융정보 제3자 제공 동의',
-    requestName: '',
+    requestName: 'financial_info_third_party_provision_consent',
     isNecessary: true,
   },
   {
     title: '개인정보 제3자 제공 동의(오픈 뱅킹)',
-    requestName: '',
+    requestName: 'open_banking_personal_info_third_party_provision_consent',
     isNecessary: true,
   },
   {
     title: '본인인증, 전자서명을 위한 개인정보 제3자 제공 동의',
-    requestName: '',
+    requestName: 'personal_info_third_party_consent_for_e_signature',
     isNecessary: true,
   },
 ]
