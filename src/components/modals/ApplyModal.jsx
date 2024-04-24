033a0148a36cfb6c41cd87728f621eae6aeb85e2
zzzdks760 <zzzdks760@naver.com>
2024-04-24 12:32:18 +0900

    ST-83 Feat: 보험신청 시 보험 이력저장 api호출 후 보험사로 연결 추가

--- src/components/modals/ApplyModal.jsx
+++ src/components/modals/ApplyModal.jsx
@@ -12,12 +12,71 @@ import {
   Checkbox,
   Image,
   Link,
+  Stack,
 } from '@chakra-ui/react'
 import BasicButton from '../buttons/BasicButton'
+import axios from 'axios'
+import { getAccessToken } from '../../utils/tokenStore'
+import { useNavigate } from 'react-router-dom'
 
-const ApplyModal = ({ responseData, isOpen, onClose, formatDate }) => {
+const BASE_URL = import.meta.env.VITE_SERVER_URL
+
+const ApplyModal = ({
+  responseData,
+  isOpen,
+  onClose,
+  formatDate,
+  userInfoData,
+  flightInsuranceId,
+}) => {
+  const [checkedItems, setCheckedItems] = React.useState([false, false])
+
+  const allChecked = checkedItems.every(Boolean)
+  const isIndeterminate = checkedItems.some(Boolean) && !allChecked
+  const accessToken = getAccessToken()
+  const navigate = useNavigate()
+
+  const handleConnect = async () => {
+    if (!allChecked) {
+      alert('필수 항목의 동의여부를 확인해 주세요.')
+    } else {
+      try {
+        const response = await axios.post(
+          `${BASE_URL}/api/v1/flight-insurance-subscriptions`,
+          {
+            flightInsuranceId: flightInsuranceId,
+            coverageStartAt: responseData.coverageStartAt,
+            coverageEndAt: responseData.coverageEndAt,
+            totalPremium: responseData.premium,
+            coverageDetailsTermsContent: checkedItems[0],
+            consentPersonalInfo: checkedItems[1],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        )
+        if (response.status === 200) {
+          window.location.href = responseData.companyUrl
+        }
+      } catch (error) {
+        if (error.response.status >= 400 && error.response.status < 600) {
+          alert('로그인 정보를 불러오는데 실패했습니다. 다시 로그인해주세요.')
+          navigate('/flight-insurance')
+        }
+      }
+    }
+  }
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onClose(onClose)
+        setCheckedItems([false, false])
+      }}
+    >
       <ModalOverlay />
       <ModalContent maxW={'550px'}>
         <ModalHeader>
@@ -40,32 +99,38 @@ const ApplyModal = ({ responseData, isOpen, onClose, formatDate }) => {
         <ModalBody>
           <Flex flexDirection={'column'} px={'50px'} py={8}>
             <Flex justifyContent={'space-between'} flexDirection={'row'}>
-              <Text>가입 시작일</Text>
-              <Text>{formatDate(responseData?.coverageStartAt)}</Text>
+              <Text fontWeight={'bold'}>가입 시작일</Text>
+              <Text fontWeight={'bold'}>
+                {formatDate(responseData?.coverageStartAt)}
+              </Text>
             </Flex>
             <Flex
               justifyContent={'space-between'}
               flexDirection={'row'}
               mt={'20px'}
             >
-              <Text>가입 종료일</Text>
-              <Text>{formatDate(responseData?.coverageEndAt)}</Text>
+              <Text fontWeight={'bold'}>가입 종료일</Text>
+              <Text fontWeight={'bold'}>
+                {formatDate(responseData?.coverageEndAt)}
+              </Text>
             </Flex>
             <Flex
               justifyContent={'space-between'}
               flexDirection={'row'}
               mt={'20px'}
             >
-              <Text>가입자</Text>
-              <Text>홍길동</Text>
+              <Text fontWeight={'bold'}>가입자</Text>
+              <Text
+                fontWeight={'bold'}
+              >{`${userInfoData.name} (${userInfoData.gender === 'MALE' ? '남성' : '여성'})`}</Text>
             </Flex>
             <Flex
               justifyContent={'space-between'}
               flexDirection={'row'}
               mt={'20px'}
             >
-              <Text>생년월일</Text>
-              <Text>1998년01월01일</Text>
+              <Text fontWeight={'bold'}>생년월일</Text>
+              <Text fontWeight={'bold'}>{userInfoData.birthDay}</Text>
             </Flex>
             <Flex justifyContent={'center'} mt={'30px'}>
               <Box
@@ -76,15 +141,15 @@ const ApplyModal = ({ responseData, isOpen, onClose, formatDate }) => {
               >
                 <Flex
                   justifyContent={'space-between'}
-                  px={'25px'}
+                  px={'10px'}
                   alignItems={'center'}
                   mt={'12px'}
                 >
-                  <Text fontSize={'21px'} fontWeight={'bold'}>
+                  <Text fontSize={'23px'} fontWeight={'bold'}>
                     예상 보험료
                   </Text>
                   {responseData?.premium && (
-                    <Text fontSize={20} fontWeight={'bold'}>
+                    <Text fontSize={23} fontWeight={'bold'}>
                       {`${responseData.premium.toLocaleString()}원`}
                     </Text>
                   )}
@@ -92,9 +157,40 @@ const ApplyModal = ({ responseData, isOpen, onClose, formatDate }) => {
               </Box>
             </Flex>
             <Flex flexDirection={'column'} pt={'50px'}>
-              <Checkbox defaultChecked>개인정보 수집 및 이용동의</Checkbox>
-              <Checkbox defaultChecked>개인정보 수집 및 이용동의</Checkbox>
-              <Checkbox defaultChecked>개인정보 수집 및 이용동의</Checkbox>
+              <Checkbox
+                isChecked={allChecked}
+                isIndeterminate={isIndeterminate}
+                borderColor={'gray.100'}
+                onChange={(e) => {
+                  return setCheckedItems([e.target.checked, e.target.checked])
+                }}
+              >
+                <Text fontSize={'16px'}>모든 필수 항목에 대한 전체 동의</Text>
+              </Checkbox>
+              <Stack pl={6} mt={1} spacing={1}>
+                <Checkbox
+                  borderColor={'gray.100'}
+                  isChecked={checkedItems[0]}
+                  onChange={(e) => {
+                    return setCheckedItems([e.target.checked, checkedItems[1]])
+                  }}
+                >
+                  <Text fontSize={'16px'}>
+                    (필수) 가입하는 보험의 보장내용, 주요내용 확인 동의
+                  </Text>
+                </Checkbox>
+                <Checkbox
+                  borderColor={'gray.100'}
+                  isChecked={checkedItems[1]}
+                  onChange={(e) => {
+                    return setCheckedItems([checkedItems[0], e.target.checked])
+                  }}
+                >
+                  <Text fontSize={'16px'}>
+                    (필수) 개인정보 수집 및 이용, 제3자 제공 동의
+                  </Text>
+                </Checkbox>
+              </Stack>
             </Flex>
           </Flex>
         </ModalBody>
@@ -106,6 +202,7 @@ const ApplyModal = ({ responseData, isOpen, onClose, formatDate }) => {
               bgColor={'main'}
               color={'white'}
               _hover={{}}
+              onClick={handleConnect}
             >
               <Link>보험사로 연결</Link>
             </BasicButton>
