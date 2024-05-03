84c8cff2171a74674ef000101f8c6663ebd47920
zzzdks760 <zzzdks760@naver.com>
2024-05-03 10:31:12 +0900

    Fix: 여행자보험 카드 가운데 정렬되도록 수정

--- src/pages/FlightInsurance.jsx
+++ src/pages/FlightInsurance.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Input, Select, Text } from '@chakra-ui/react'
+import { Box, Flex, Grid, Input, Select, Text } from '@chakra-ui/react'
 import axios from 'axios'
 
 import React, { useEffect, useState } from 'react'
@@ -191,192 +191,197 @@ const FlightInsurance = () => {
 
   return (
     <>
-      {/* 검색폼 */}
       <Flex
-        m={'0 auto'}
-        w="800px"
-        borderRadius="10px"
-        border="1px solid"
-        borderColor="gray.100"
-        backgroundColor="white"
         flexDirection={'column'}
+        justifyContent={'center'}
         alignItems={'center'}
-        mt={'50px'}
-        p={'40px'}
-        py={'40px'}
-      >
-        <Flex w={'90%'} justifyContent={'space-between'} alignItems={'center'}>
-          <Input
-            mr={'30px'}
-            width={'309px'}
-            height={'49px'}
-            borderRadius={'10px'}
-            borderWidth={'1px'}
-            borderColor={'gray.200'}
-            focusBorderColor={'main'}
-            type="datetime-local"
-            placeholder="출발 날짜"
-            min={new Date(today.getTime() + 60 * 60 * 1000)
-              .toISOString()
-              .slice(0, 16)}
-            max={getMaxDate()}
-            value={departAt}
-            onChange={handleDepartAtChange}
-          />
-          <Input
-            width={'309px'}
-            height={'49px'}
-            borderRadius={'10px'}
-            borderWidth={'1px'}
-            borderColor={'gray.200'}
-            focusBorderColor={'main'}
-            type="datetime-local"
-            placeholder="도착 날짜"
-            min={new Date(today.getTime() + 24 * 60 * 60 * 1000)
-              .toISOString()
-              .slice(0, 16)}
-            max={getMaxDate()}
-            value={arrivalAt}
-            onChange={handleArrivalAtChange}
-          />
-        </Flex>
-
-        <Box marginTop={'30px'} display={'flex'} alignItems={'center'}>
-          <Input
-            marginRight={'30px'}
-            width={'309px'}
-            height={'49px'}
-            borderRadius={'10px'}
-            borderWidth={'1px'}
-            borderColor={'gray.200'}
-            focusBorderColor={'main'}
-            type="text"
-            maxLength={8}
-            placeholder="생년월일 ex)19980517"
-            value={birthDay}
-            onChange={handleBirthDayChange}
-          />
-          <Box>
-            <BasicButton
-              width={'154px'}
-              height={'49px'}
-              borderColor={'gray.200'}
-              borderRadius={'10px 0px 0px 10px'}
-              border={'1px solid'}
-              color={gender === Gender.MALE ? 'main' : 'gray.200'}
-              bgColor={'white'}
-              _hover={{ color: 'main' }}
-              onClick={() => {
-                handleGenderClick(Gender.MALE)
-              }}
-            >
-              남
-            </BasicButton>
-            <BasicButton
-              width={'154px'}
-              height={'49px'}
-              borderRadius={'0px 10px 10px 0px'}
-              color={gender === Gender.FEMALE ? 'main' : 'gray.200'}
-              border={'1px solid'}
-              bgColor={'white'}
-              _hover={{ color: 'main' }}
-              onClick={() => {
-                handleGenderClick(Gender.FEMALE)
-              }}
-            >
-              여
-            </BasicButton>
-          </Box>
-        </Box>
-
-        <Box>
-          <Box
-            display={'flex'}
-            textAlign={'center'}
-            marginTop={'30px'}
-            marginRight={'210px'}
-          >
-            <Text pt={'1px'} fontSize={'lg'} mr={'20px'}>
-              세부특약
-            </Text>
-            {/* 각 특약 조항 버튼을 배열로 매핑하여 동적으로 생성 */}
-            {isClicked.map((clicked, index) => {
-              return (
-                <BasicButton
-                  marginRight={'15px'}
-                  width={'100px'}
-                  height={'30px'}
-                  bgColor={'white'}
-                  border={'1px solid'}
-                  color={clicked ? 'main' : 'gray.200'}
-                  _hover={{ color: 'main' }}
-                  key={index}
-                  onClick={() => {
-                    return handleContractClick(index)
-                  }}
-                >
-                  {index === 0
-                    ? '항공기지연'
-                    : index === 1
-                      ? '여권분실'
-                      : '식중독'}
-                </BasicButton>
-              )
-            })}
-          </Box>
-        </Box>
-
-        <Box width="100%" textAlign={'center'} mt={'30px'}>
-          <BasicButton
-            color="white"
-            width="100%"
-            bgColor="main"
-            height="45px"
-            _hover={{}}
-            onClick={handleSearch}
-          >
-            검색하기
-          </BasicButton>
-        </Box>
-      </Flex>
-      {/* 플랜선택 */}
-      <Flex
-        justifyContent={'space-between'} // 텍스트와 셀렉트를 각각 왼쪽과 오른쪽에 정렬합니다.
-        // alignItems={'flex-start'} // 수직으로는 위로 정렬합니다.
         marginTop={'50px'}
       >
-        <Box>
-          <Text color={'gray.600'} fontSize={'xl'}>
-            조회결과
-          </Text>
-          <Text>
-            {`총 ${responseData.length}개의 여행자보험상품이 조회되었습니다.`}
-          </Text>
-        </Box>
-        <Select
-          width={115}
-          height={42}
-          borderWidth={2}
-          borderColor={'gray.200'}
-          value={planName}
-          onChange={handlePlanClick}
+        {/* 검색폼 */}
+        <Flex
+          m={'0 auto'}
+          w="800px"
+          borderRadius="10px"
+          border="1px solid"
+          borderColor="gray.100"
+          backgroundColor="white"
+          flexDirection={'column'}
+          alignItems={'center'}
+          mt={'50px'}
+          p={'40px'}
+          py={'40px'}
         >
-          <option value="standard">표준플랜</option>
-          <option value="advanced">고급플랜</option>
-        </Select>
-      </Flex>
+          <Flex
+            w={'90%'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+          >
+            <Input
+              mr={'30px'}
+              width={'309px'}
+              height={'49px'}
+              borderRadius={'10px'}
+              borderWidth={'1px'}
+              borderColor={'gray.200'}
+              focusBorderColor={'main'}
+              type="datetime-local"
+              placeholder="출발 날짜"
+              min={new Date(today.getTime() + 60 * 60 * 1000)
+                .toISOString()
+                .slice(0, 16)}
+              max={getMaxDate()}
+              value={departAt}
+              onChange={handleDepartAtChange}
+            />
+            <Input
+              width={'309px'}
+              height={'49px'}
+              borderRadius={'10px'}
+              borderWidth={'1px'}
+              borderColor={'gray.200'}
+              focusBorderColor={'main'}
+              type="datetime-local"
+              placeholder="도착 날짜"
+              min={new Date(today.getTime() + 24 * 60 * 60 * 1000)
+                .toISOString()
+                .slice(0, 16)}
+              max={getMaxDate()}
+              value={arrivalAt}
+              onChange={handleArrivalAtChange}
+            />
+          </Flex>
 
-      {/* 보험상품 카드 */}
-      <Flex
-        flexWrap={'wrap'}
-        my={8}
-        justifyContent={'flex-start'}
-        flexDirection={'row'}
-        gap={10}
-      >
-        {responseData.map((card, index) => {
-          return <FlightInsuranceCard key={index} card={card} />
-        })}
+          <Box marginTop={'30px'} display={'flex'} alignItems={'center'}>
+            <Input
+              marginRight={'30px'}
+              width={'309px'}
+              height={'49px'}
+              borderRadius={'10px'}
+              borderWidth={'1px'}
+              borderColor={'gray.200'}
+              focusBorderColor={'main'}
+              type="text"
+              maxLength={8}
+              placeholder="생년월일 ex)19980517"
+              value={birthDay}
+              onChange={handleBirthDayChange}
+            />
+            <Box>
+              <BasicButton
+                width={'154px'}
+                height={'49px'}
+                borderColor={'gray.200'}
+                borderRadius={'10px 0px 0px 10px'}
+                border={'1px solid'}
+                color={gender === Gender.MALE ? 'main' : 'gray.200'}
+                bgColor={'white'}
+                _hover={{ color: 'main' }}
+                onClick={() => {
+                  handleGenderClick(Gender.MALE)
+                }}
+              >
+                남
+              </BasicButton>
+              <BasicButton
+                width={'154px'}
+                height={'49px'}
+                borderRadius={'0px 10px 10px 0px'}
+                color={gender === Gender.FEMALE ? 'main' : 'gray.200'}
+                border={'1px solid'}
+                bgColor={'white'}
+                _hover={{ color: 'main' }}
+                onClick={() => {
+                  handleGenderClick(Gender.FEMALE)
+                }}
+              >
+                여
+              </BasicButton>
+            </Box>
+          </Box>
+
+          <Box>
+            <Box
+              display={'flex'}
+              textAlign={'center'}
+              marginTop={'30px'}
+              marginRight={'210px'}
+            >
+              <Text pt={'1px'} fontSize={'lg'} mr={'20px'}>
+                세부특약
+              </Text>
+              {/* 각 특약 조항 버튼을 배열로 매핑하여 동적으로 생성 */}
+              {isClicked.map((clicked, index) => {
+                return (
+                  <BasicButton
+                    marginRight={'15px'}
+                    width={'100px'}
+                    height={'30px'}
+                    bgColor={'white'}
+                    border={'1px solid'}
+                    color={clicked ? 'main' : 'gray.200'}
+                    _hover={{ color: 'main' }}
+                    key={index}
+                    onClick={() => {
+                      return handleContractClick(index)
+                    }}
+                  >
+                    {index === 0
+                      ? '항공기지연'
+                      : index === 1
+                        ? '여권분실'
+                        : '식중독'}
+                  </BasicButton>
+                )
+              })}
+            </Box>
+          </Box>
+
+          <Box width="100%" textAlign={'center'} mt={'30px'}>
+            <BasicButton
+              color="white"
+              width="100%"
+              bgColor="main"
+              height="45px"
+              _hover={{}}
+              onClick={handleSearch}
+            >
+              검색하기
+            </BasicButton>
+          </Box>
+        </Flex>
+        {/* 플랜선택 */}
+        <Flex
+          justifyContent={'space-between'} // 텍스트와 셀렉트를 각각 왼쪽과 오른쪽에 정렬합니다.
+          marginTop={'50px'}
+          width={'1000px'} // 너비를 명시적으로 설정하여 부모 요소의 너비에 맞춰 정렬될 수 있도록 합니다.
+        >
+          <Box>
+            <Text color={'gray.600'} fontSize={'xl'}>
+              조회결과
+            </Text>
+            <Text>
+              {`총 ${responseData.length}개의 여행자보험상품이 조회되었습니다.`}
+            </Text>
+          </Box>
+          <Select
+            width={115}
+            height={42}
+            borderWidth={2}
+            borderColor={'gray.200'}
+            value={planName}
+            onChange={handlePlanClick}
+          >
+            <option value="standard">표준플랜</option>
+            <option value="advanced">고급플랜</option>
+          </Select>
+        </Flex>
+
+        {/* 보험상품 카드 */}
+        <Grid templateColumns="repeat(3, 1fr)" my={8} gap={10}>
+          {responseData.map((card, index) => {
+            return <FlightInsuranceCard key={index} card={card} />
+          })}
+        </Grid>
       </Flex>
     </>
   )
